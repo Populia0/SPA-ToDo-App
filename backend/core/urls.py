29fbfaf46eb235ef1b69b8f5db3c7bba20a96eff
Populia0 <populia000@gmail.com>
2024-06-14 10:51:55 +0500

    настройка фронта

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,9 +1,12 @@
-from .views import *
-from django.urls import path
- 
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from core import views
+
+router = routers.DefaultRouter()
+router.register(r'todos', views.TodoView, 'todo')
+
 urlpatterns = [
-    path('', home),
-    path('task', postTask),
-    path('task/delete/<int:pk>/', deleteTask),
-    path('task/mark/<int:pk>/', markCompleted)
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,43 +1,8 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
-from .models import *
-from .serializers import *
- 
- 
-@api_view(['GET'])
-def home(request):
-    taskobj = Todo.objects.all()
-    print(taskobj)
-    serializer = Todo(taskobj, many=True)
-    return Response({'status': 200, 'payload': serializer.data})
- 
- 
-@api_view(['POST'])
-def markCompleted(request, pk):
-    try:
-        instance = Todo.objects.get(pk=pk)
-    except Todo.DoesNotExist:
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    instance.completed = True
-    instance.save()
-    return Response(status=status.HTTP_204_NO_CONTENT)
- 
- 
-@api_view(['POST'])
-def postTask(request):
-    serializer = TodoSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
- 
- 
-@api_view(['DELETE'])
-def deleteTask(request, pk):
-    try:
-        instance = Todo.objects.get(pk=pk)
-    except Todo.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    instance.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
+from rest_framework import viewsets
+from .serializers import TodoSerializer
+from .models import Todo
+
+class TodoView(viewsets.ModelViewSet):
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all()
